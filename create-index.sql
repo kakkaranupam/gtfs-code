8b338e125c2563ed528023e5e4fb6c2d04c01718
Anupam Kakkar <kakkaranupam@hotmail.com>
2019-01-11 22:34:35 +0100

    Static 90%, PB search
    Done with almost 90% of the static dataset, started parsing the pb files.

---
+++ create-index.sql
@@ -0,0 +1,57 @@
+USE gtfs;
+-- 1, 2
+CREATE INDEX route__route_id
+    ON routes(route_id)
+    USING HASH;
+
+CREATE INDEX route__agency_id
+    ON routes(agency_id)
+    USING HASH;
+
+-- 3, 4, 5
+CREATE INDEX trips__route_id
+    ON trips(route_id)
+    USING HASH;
+
+CREATE INDEX trips__service_id
+    ON trips(service_id)
+    USING HASH;
+
+CREATE INDEX trips__trip_id
+    ON trips(trip_id)
+    USING HASH;
+
+-- 6
+CREATE INDEX stop_times__stop_id
+    ON stop_times(stop_id)
+    USING HASH;
+
+-- REST
+CREATE INDEX transfers__stop_ids
+    ON transfers(from_stop_id, to_stop_id)
+    USING HASH;
+
+CREATE INDEX transfers__stop_ids_reversed
+    ON transfers(to_stop_id, from_stop_id)
+    USING HASH;
+
+CREATE INDEX transfers__route_ids
+    ON transfers(from_route_id, to_route_id)
+    USING HASH;
+
+CREATE INDEX transfers__route_ids_reversed
+    ON transfers(to_route_id, from_route_id)
+    USING HASH;
+
+CREATE INDEX transfers__trip_ids
+    ON transfers(from_route_id, to_route_id)
+    USING HASH;
+
+CREATE INDEX transfers__trip_ids_reversed
+    ON transfers(to_trip_id, from_trip_id)
+    USING HASH;
+	
+-- NEW ONES
+CREATE INDEX stop_times__trip_id
+    ON stop_times(trip_id)
+    USING HASH;
---
+++ create-tables.sql
@@ -0,0 +1,117 @@
+DROP SCHEMA IF EXISTS gtfs;
+
+CREATE SCHEMA gtfs;
+
+USE gtfs;
+
+CREATE TABLE IF NOT EXISTS agency (
+    agency_id               VARCHAR(20)     NOT NULL,
+    agency_name             VARCHAR(255)    NOT NULL,
+    agency_url              VARCHAR(255)    NOT NULL,
+    agency_timezone         VARCHAR(50)     NOT NULL,
+    agency_phone            VARCHAR(40),
+
+    PRIMARY KEY (agency_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS routes (
+    route_id                INT UNSIGNED    NOT NULL,
+    agency_id               VARCHAR(20)     NOT NULL,
+    route_short_name        VARCHAR(60)     NOT NULL,
+    route_long_name         VARCHAR(100)    NOT NULL,
+    route_desc              VARCHAR(255),
+    route_type              TINYINT         NOT NULL,
+    route_color             VARCHAR(10),
+    route_text_color        VARCHAR(6),
+    route_url               VARCHAR(255),
+
+    PRIMARY KEY (route_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- IDX 1 and 2
+
+CREATE TABLE IF NOT EXISTS shapes (
+    shape_id                INT UNSIGNED    NOT NULL,
+    shape_pt_sequence       SMALLINT        NOT NULL,
+    shape_pt_lat            VARCHAR(20)     NOT NULL,
+    shape_pt_lon            VARCHAR(20)     NOT NULL,
+    shape_dist_traveled     SMALLINT        NOT NULL,
+
+    PRIMARY KEY (shape_id, shape_pt_sequence)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS calendar_dates (
+    service_id              INT UNSIGNED    NOT NULL,
+    cal_date				VARCHAR(8),
+    exception_type          TINYINT,
+
+    PRIMARY KEY (service_id, cal_date)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS trips (
+    route_id                INT UNSIGNED    NOT NULL,
+    service_id              INT UNSIGNED    NOT NULL,
+    trip_id                 INT UNSIGNED    NOT NULL,
+    realtime_trip_id        VARCHAR(30),
+    trip_headsign           VARCHAR(80),
+    trip_short_name         VARCHAR(30),
+    trip_long_name          VARCHAR(100),
+    direction_id            BOOLEAN,
+    block_id                VARCHAR(20),
+    shape_id                MEDIUMINT,
+    wheelchair_accessible   TINYINT,
+    bikes_allowed           TINYINT,
+	trip_date				VARCHAR(8),
+
+	PRIMARY KEY (route_id, service_id, trip_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- IDX 3, 4, 5
+
+CREATE TABLE IF NOT EXISTS stops (
+    stop_id                 VARCHAR(20)     NOT NULL,
+    stop_code               VARCHAR(20),
+    stop_name               VARCHAR(80)     NOT NULL,
+    stop_lat                VARCHAR(20)     NOT NULL,
+    stop_lon                VARCHAR(20)     NOT NULL,
+    location_type           BOOLEAN,
+    parent_station          BOOLEAN,
+    stop_timezone           VARCHAR(50),
+    wheelchair_boarding     TINYINT,
+    platform_code           VARCHAR(20),
+	zone_id					VARCHAR(20),
+	
+    PRIMARY KEY (stop_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS stop_times (
+    trip_id                 INT UNSIGNED    NOT NULL,
+    stop_sequence           SMALLINT        NOT NULL,
+    stop_id                 VARCHAR(20)		NOT NULL,
+    stop_headsign           VARCHAR(80),
+    arrival_time            VARCHAR(10)     NOT NULL,
+    departure_time          VARCHAR(10)     NOT NULL,
+    pickup_type             TINYINT,
+    drop_off_type           TINYINT,
+    timepoint               BOOLEAN,
+    shape_dist_traveled     DOUBLE,
+    fare_units_traveled     DOUBLE,
+	trip_date				VARCHAR(8),
+
+    PRIMARY KEY (trip_id, stop_id, stop_sequence, arrival_time, departure_time)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- IDX 6
+
+CREATE TABLE IF NOT EXISTS transfers (
+    from_stop_id            VARCHAR(20)     NOT NULL,
+    to_stop_id              VARCHAR(20)     NOT NULL,
+    from_route_id           INT UNSIGNED,
+    to_route_id             INT UNSIGNED,
+    from_trip_id            INT UNSIGNED,
+    to_trip_id              INT UNSIGNED,
+    transfer_type           TINYINT         NOT NULL,
+--    min_transfer_time       SMALLINT,
+
+    PRIMARY KEY (from_stop_id, to_stop_id, from_route_id, to_route_id, from_trip_id, to_trip_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
