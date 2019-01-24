f63a6ec6e9efa0dbb000868d03b7a833d19da298
Anupam Kakkar <kakkaranupam@hotmail.com>
2019-01-24 03:19:18 +0100

    sns integration
    Seaborn and Pandas for plots.

--- create-index.sql
+++ create-index.sql
@@ -1,32 +1,31 @@
 USE gtfs;
 -- 1, 2
 CREATE INDEX route__route_id
-    ON routes(route_id)
-    USING HASH;
+ON routes(route_id);
 
 CREATE INDEX route__agency_id
-    ON routes(agency_id)
-    USING HASH;
+ON routes(agency_id);
 
 -- 3, 4, 5
 CREATE INDEX trips__route_id
-    ON trips(route_id)
-    USING HASH;
+ON trips(route_id);
 
 CREATE INDEX trips__service_id
-    ON trips(service_id)
-    USING HASH;
+ON trips(service_id);
 
 CREATE INDEX trips__trip_id
-    ON trips(trip_id)
-    USING HASH;
+ON trips(trip_id);
 
 -- 6
 CREATE INDEX stop_times__stop_id
-    ON stop_times(stop_id)
-    USING HASH;
-	
+ON stop_times(stop_id);
+
 -- NEW ONES
 CREATE INDEX stop_times__trip_id
-    ON stop_times(trip_id)
-    USING HASH;
+ON stop_times(trip_id);
+
+CREATE INDEX trip_updates__trip_id
+ON trip_updates(trip_id);
+
+CREATE INDEX trip_updates__stop_id
+ON trip_updates(stop_id);
